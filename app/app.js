d2574c8dcd6616e88596a90640bf0b95b88544a8
David Patiño <david.andrespr5@gmail.com>
2022-04-28 16:54:30 -0500

    4. Agregando una nueva ruta

--- app/app.js
+++ app/app.js
@@ -10,7 +10,10 @@ const port=3000
 app.get('/',(req,res) => {
     res.send('Hello World!')
 })
-
+//localhost:3000/launchx
+app.get('/launchx',(req,res)=>{
+    res.send("Bienvenido a LaunchX")
+})
 // Con esto incializamos esta app
 app.listen(port,() =>{
     console.log(`Example app listening on port: ${port}`)
